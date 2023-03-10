6ace076a680fb31f7f20ef80c7413f1342b7eed4
Richard Lucas <richardluca@gmail.com>
2023-03-11 00:05:19 +0100

    finished layout SingleRecipe

--- src/pages/SingleRecipe/SingleRecipe.js
+++ src/pages/SingleRecipe/SingleRecipe.js
@@ -4,33 +4,90 @@ import parse from "html-react-parser";
 import RecipeImage from "../../components/RecipeImage";
 import styles from "./SingleRecipe.module.scss";
 import { useState } from "react";
+import { GiCheckMark } from "react-icons/gi";
+import { FaRegHandPointRight } from "react-icons/fa";
+import { GiCarrot } from "react-icons/gi";
+import { nanoid } from "nanoid";
+import { isEmpty } from "lodash";
 
 const SingleRecipe = () => {
-  console.log(dataTest);
-  const {
+  let {
     title,
     image,
     summary,
-    vegan: { veganstat: vegan },
-    dairyFree: { dairyFreestat: dairyFree },
-    glutenFree: { glutenFreestat: glutenFree },
-    veryHealthy: veryHealthy,
-    cheap: cheap,
-    sustainable: sustainable,
+    extendedIngredients: ingredients,
+    analyzedInstructions: [{ steps }],
   } = dataTest;
   const [showMore, setShowMore] = useState(false);
+  let recipeStats = [];
 
-  let recipeStats = [
-    veganstat,
-    dairyFreestat,
-    glutenFreestat,
-    veryHealthy,
-    cheap,
-    sustainable,
-  ];
-  console.log("recipeStats", recipeStats);
-  const recipeStatsToDisplay = recipeStats.filter((item) => item);
-  console.log("filter", recipeStatsToDisplay);
+  // add each recipeStat only if true
+  for (const [key, value] of Object.entries(dataTest)) {
+    if (key === "vegan" && value === true) recipeStats.push("Vegan");
+    if (key === "glutenFree" && value === true) recipeStats.push("Gluten Free");
+    if (key === "dairyFree" && value === true) recipeStats.push("Dairy Free");
+    if (key === "veryHealthy" && value === true)
+      recipeStats.push("Very Healthy");
+    if (key === "cheap" && value === true) recipeStats.push("Cheap");
+    if (key === "sustainable" && value === true)
+      recipeStats.push("Sustainable");
+  }
+
+  const recipeStatsToDisplay = recipeStats.map((item) => {
+    return (
+      <li key={nanoid()}>
+        <GiCheckMark className={styles.check} />
+        {item}
+      </li>
+    );
+  });
+
+  ingredients = ingredients.map((item) => {
+    const {
+      nameClean,
+      measures: {
+        metric: { amount, unitShort },
+      },
+    } = item;
+    return { nameClean, amount, unitShort };
+  });
+
+  const ingredientsToDisplay = ingredients.map((item) => {
+    if (item.amount) {
+      item.amount = Math.round(item.amount);
+    } else {
+      item.amount = "(amount not available)";
+    }
+
+    if (isEmpty(item.unitShort) && item.unitShort !== "")
+      item.unitShort = "(unit not available)";
+
+    if (isEmpty(item.nameClean))
+      item.nameClean = "(ingredient name not available)";
+
+    return (
+      <li key={nanoid()}>
+        <FaRegHandPointRight className={styles.check} />
+        {item.amount} {item.unitShort} {item.nameClean}
+      </li>
+    );
+  });
+
+  console.log("cookingInstructions 1", steps);
+
+  const instructionsToDisplay = steps.map((item) => {
+    const { number, step } = item;
+
+    return (
+      <li>
+        <p>
+          <span className={styles.stepNumber}>{number}.</span> {step}
+        </p>
+      </li>
+    );
+  });
+
+  console.log("cookingInstrucitons 2", steps);
 
   return (
     <article className={styles.summaryLayout}>
@@ -38,16 +95,28 @@ const SingleRecipe = () => {
       <div className={styles.imgLayout}>
         <RecipeImage image={image} title={title} />
       </div>
-      <div>
-        {showMore ? parse(summary) : parse(summary.substring(0, 400)) + "..."}{" "}
+      <section className={styles.section}>
+        <h3>Description</h3>
+        {showMore ? parse(summary) : parse(summary.substring(0, 400) + "...")}
         <button
           className={styles.showMoreBtn}
           onClick={() => setShowMore(!showMore)}
         >
           {showMore ? "Show less" : "Show more"}
         </button>
-      </div>
-      <h2>Stats</h2>
+      </section>
+      <section className={styles.section}>
+        <h3>Recipe Stats</h3>
+        <ul className={styles.statsList}>{recipeStatsToDisplay}</ul>
+      </section>
+      <section className={styles.section}>
+        <h3>Ingredients</h3>
+        <ul className={styles.ingredientsList}>{ingredientsToDisplay}</ul>
+      </section>
+      <section>
+        <h3>Cooking instructions</h3>
+        <ul className={styles.instructionsList}>{instructionsToDisplay}</ul>
+      </section>
     </article>
   );
 };
